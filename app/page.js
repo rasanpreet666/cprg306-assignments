0bfe6ffcdb865320967ffde5f19e08d8f2227f23
rasanpreet666 <157263144+rasanpreet666@users.noreply.github.com>
2025-03-12 16:12:07 -0600

    week7 Donep

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ export default function Page() {
       <p>
         <Link href="/week-2">Week 2 Assignments  </Link>
         <Link href="/week-3">Week 3 Assignments</Link>
+        <Link href="/week-7">Week 7 Assignments</Link>
       </p>
     </main>
   );
